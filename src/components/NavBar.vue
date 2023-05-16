aa8e415f83be4ee14c241aab0c104671ac0c2287
lucas-faget <lucas.faget@laposte.net>
2023-05-16 19:36:07 +0200

    begin navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,40 @@
 <script>
+import OutlineText from './OutlineText.vue'
     export default {
+        components: { OutlineText },
         name: 'NavBar'
     }
 </script>
 
 <template>
-    <div class="nav-bar">
-        <router-link to="/">Home</router-link>
-        <router-link to="/movies">Movies</router-link>
-    </div>
+    <nav class="nav-bar">
+        <div class="logo">
+            <outline-text text="VUE-MOVIES" fontSize="30" color="white"></outline-text>
+        </div>
+        <ul class="menu">
+            <router-link to="/" style="text-decoration: none;">
+                <li class="menu-item"><outline-text text="HOME" fontSize="30" color="gold"></outline-text></li>
+            </router-link>
+            <router-link to="/movies" style="text-decoration: none;">
+                <li class="menu-item"><outline-text text="Movies" fontSize="30" color="gold"></outline-text></li>
+            </router-link>
+        </ul>
+    </nav>
 </template>
 
 <style scoped>
+    .nav-bar {
+        /* background: blue; */
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 80px;
+        padding-inline: 30px;
+    }
 
+    .menu {
+        display: flex;
+        gap: 30px;
+        list-style: none;
+    }
 </style>
