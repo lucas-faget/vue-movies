b37951608198a3fa613c2a93c4a5ac35cb99f575
lucas-faget <lucas.faget@laposte.net>
2023-05-16 13:52:44 +0200

    outline text component

---
+++ src/components/OutlineText.vue
@@ -0,0 +1,46 @@
+<script>
+    export default {
+        name: 'PosterList',
+        props: ['text', 'fontSize', 'color'],
+        computed: {
+            outlineColorClass() {
+                switch (this.color) {
+                    case "gold":
+                        return "outline-gold";
+                    case "white":
+                    default:
+                        return "outline-white";
+                }
+            },
+            fontSizeStyle() {
+                return {
+                    fontSize: `${this.fontSize}px`,
+                }
+            }
+        }
+    }
+</script>
+
+<template>
+    <p :class="['outline-text', outlineColorClass]" :style="fontSizeStyle">
+        {{ text }}
+    </p>
+</template>
+
+<style scoped>
+    .outline-text {
+        margin: 0;
+        padding: 0;
+        color: #000;
+    }
+
+    .outline-white {
+        -webkit-text-stroke: 1px #fff;
+        text-stroke: 1px #fff;
+    }
+
+    .outline-gold {
+        -webkit-text-stroke: 1px var(--color-gold);
+        text-stroke: 1px var(--color-gold);
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script>
     import PosterList from '@/components/PosterList.vue';
+    import OutlineText from '@/components/OutlineText.vue';
     
     export default {
-    components: { PosterList },
+        components: { PosterList, OutlineText },
         data() {
             return {
                 movieRepository: window.movieRepository,
@@ -27,8 +28,13 @@
 
 <template>
     <h1>Home</h1>
+    <outline-text text="VUE-MOVIES" fontSize="50" color="white"></outline-text>
 
     <button type="button" @click="addMovie()">Ajouter un film</button>
 
     <poster-list :posterList="posterList"></poster-list>
 </template>
+
+<style scoped>
+    
+</style>
